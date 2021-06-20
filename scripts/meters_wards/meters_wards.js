62664e100fa1389169b9a6eec213e6ddc4052d0b
Irene de la Torre <irene.delatorre@gmail.com>
2021-06-20 23:38:18 +0200

    started the table

--- scripts/meters_wards/meters_wards.js
+++ scripts/meters_wards/meters_wards.js
@@ -4,12 +4,11 @@ class meters_by_wards {
         this.max = data[1];
         this.extentDate = extentDate;
         this.margin = {
-            t: 30,
-            l: 160,
-            r: 30,
-            b: 25
+            t: 1.5,
+            b: 1.5
         };
         this.scaleColor = scaleColor;
+        this.format = d3.format(",");
 
         this.t = this.extentDate[1];
         this.dateSummary = (Array.from(this.data, (d) => {
@@ -18,31 +17,100 @@ class meters_by_wards {
                 value: d[this.t]
             }
         })
-        ).sort((a,b) => a.value - b.value);
+        ).sort((a,b) => b.value - a.value);
 
         console.log(this.dateSummary)
         this.wards = this.dateSummary.map(d => d.key);
 
-        this.width = document.getElementById('meters-by-district-plot').clientWidth - this.margin.r - this.margin.l;
-        this.height = document.getElementById('meters-by-district-plot').clientHeight - this.margin.t - this.margin.b;
+        this.table = d3.select("#meters-by-district-table").selectAll('tbody');
 
+        this.width = document.getElementById('barchart-head').clientWidth;
+        this.height = 20 - this.margin.t - this.margin.b;
         this.scaleX = d3.scaleLinear()
             .domain([0, this.max]).nice()
-            .range([0, this.width]);
+            .range([0, 232]);
 
-        this.scaleY = d3.scaleBand()
-            .domain(this.wards)
-            .range([this.height, 0])
-            .padding(0.1);
+        this.createTable();
+        // // svg
+        // this.plot = d3.select('#meters-by-district-plot')
+        //     .append('svg')
+        //     .attr('width', this.width + this.margin.r + this.margin.l)
+        //     .attr('height', this.height + this.margin.t + this.margin.b);
 
+        // this.createBarChart();
+        
 
-        // svg
-        this.plot = d3.select('#meters-by-district-plot')
-            .append('svg')
-            .attr('width', this.width + this.margin.r + this.margin.l)
-            .attr('height', this.height + this.margin.t + this.margin.b);
+    }
 
-        this.createBarChart();
+    createTable() {
+        const thisRow = this.table
+            .selectAll('.row')
+            .data(this.dateSummary)
+            .join('tr')
+            .attr('class', d => `${d.key}_row row`);
+
+        thisRow
+            .selectAll('td')
+            .data(d => [{
+                value: d.key,
+                class: 'name'
+            }, {
+                value: Math.round(10000 * d.value / this.max) / 100,
+                class: 'perc'
+            }, {
+                value: '',
+                class: 'svg',
+                meters: d.value
+            }, {
+                value: this.format(d.value),
+                class: 'meters'
+            }])
+            .join('td')
+            .attr('class', d => d.class)
+            .html(d => d.value);
+
+        const plotSVG = thisRow
+            .selectAll('.svg')
+            .selectAll('svg')
+            .data(d => [d])
+            .join('svg')
+            .attr('class', d => `svg_${d.key}`)
+            .attr('width', this.width)
+            .attr('height', this.height + this.margin.t + this.margin.b)
+            .selectAll('rect')
+            .data(d => [d])
+            .join('rect')
+            .attr('x', 0)
+            .attr('y', this.margin.t)
+            .attr('width', d => this.scaleX(d.meters))
+            .attr('height', this.height)
+            .style('fill', d => this.scaleColor(100 * d.meters / this.max))
+
+        // thisRow
+        //     .selectAll('.perc')
+        //     .data(d => [d])
+        //     .join('td')
+        //     .attr('class', 'perc')
+        //     .html(d => Math.round(10000 * d.value / this.max) / 100);
+
+        // this.bars = thisRow
+        //     .selectAll('.bars')
+        //     .data(d => [d])
+        //     .join('td')
+        //     .attr('class', 'bars')
+        //     .selectAll('svg')
+        //     .data(d => [d])
+        //     .join('svg')
+        //     .call(this.createBar(d));
+
+        // thisRow
+        //     .selectAll('.meters')
+        //     .data(d => [d])
+        //     .join('td')
+        //     .attr('class', 'meters')
+        //     .html(d => d.value);
+    }
+    createBar(d) {
 
     }
 
